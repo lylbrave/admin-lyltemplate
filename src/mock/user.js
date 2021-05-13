51200a5bdc4885f96c3611cdae1dbe67cadcdd51
李玉良 <38196886+lylbrave@users.noreply.github.com>
2021-05-13 10:24:21 +0800

    mod mock

--- src/mock/user.js
+++ src/mock/user.js
@@ -8,39 +8,85 @@ export default {
         token: "editor-token",
       },
     };
-    const { username } =JSON.parse(config.body) ;
+    const {
+      username
+    } = JSON.parse(config.body);
     const token = tokens[username];
-    console.log(config);
+
     return {
       code: 20000,
       data: token,
     };
   },
-  getUserInfo: () => {
+  getUserInfo: (config) => {
+    console.log(config);
     const users = {
       "admin-token": {
         roles: ["admin"],
         introduction: "I am a super administrator",
-        avatar:
-          "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
+        avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
         name: "Super Admin",
+        menuList: [{
+            path: "/home",
+            name: "home",
+            label: "首页",
+            icon: "s-home",
+          },
+          {
+            path: "/video",
+            name: "video",
+            label: "视频管理",
+            icon: "video-play",
+          },
+          {
+            path: "/userManager",
+            name: "userManager",
+            label: "用户管理",
+            icon: "user",
+          },
+          {
+            label: "其他",
+            name: "other",
+            icon: "orange",
+            children: [{
+                path: "/page1",
+                name: "page1",
+                label: "页面1"
+              },
+              {
+                path: "/page2",
+                name: "page2",
+                label: "页面2"
+              },
+            ],
+          },
+        ],
+        routerList: ['home', 'video', 'userManager', 'other']
       },
       "editor-token": {
         roles: ["editor"],
         introduction: "I am an editor",
-        avatar:
-          "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
+        avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
         name: "Normal Editor",
+        menuList: [{
+          path: "/home",
+          name: "home",
+          label: "首页",
+          icon: "s-home",
+        }],
+        routerList: ['home']
       },
     };
-    const { token } = config.query;
+    console.log(config)
+    const
+      token = config.url.split('?')[1].split('=')[1];
     const info = users[token];
     return {
       code: 20000,
       data: info,
     };
   },
-  getLoginOut:()=>{
+  getLoginOut: () => {
     return {
       code: 20000,
       data: null,
